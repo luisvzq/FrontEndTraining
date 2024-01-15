fd4e1763566056cb51c3fbe05ee3352ee0359304
David <142534572+Barreira83@users.noreply.github.com>
2024-01-15 17:30:56 +0100

    Primer commit TrainingDetails

--- src/pages/TrainingDetail.jsx
+++ src/pages/TrainingDetail.jsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from "react";
 import Menu from "../components/Menu";
+import { useParams } from "react-router-dom";
 
 const TrainingDetail = () => {
+const [dataTraining, setDataTraining] = useState( 
+  {
+  "name": "Sentadillas",
+  "description": "5 series de 12 repeticiones",
+  "photo": "defaultAvatar.jpg",
+  "typology": "Fuerza",
+  "muscle_group": "Pierna",
+  "created_at": "2024-01-14T16:04:31.000Z"
+}
+
+)
+  const  {id}  = useParams();
+
+  console.log("trainingID: ",id);
+
+
+    useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:3001/training/${id}`);
+        
+        if (response.ok) {
+          const body = await response.json();
+          console.log("response body", body);
+          setDataTraining(body.data);
+        } else {
+          const body = await response.json();
+          console.error("ERROR",body.message);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [id]);
+
   return (
     <>
       <Menu />
       <div>Pagina Training Detail</div>
+      <h1>Title: {dataTraining.name}</h1>
+      <img src={`http://localhost:3001/${dataTraining.photo}`} alt="Foto de entreno" />
+      <p>Description: {dataTraining.description}</p>
+      <button>Typology: {dataTraining.typology}</button>
+      <button>Muscle group: {dataTraining.muscle_group}</button>
+      <p>Likes: {dataTraining.allLikes}</p>
     </>
   );
 };
