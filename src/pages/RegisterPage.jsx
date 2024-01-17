1a165c396912940ffad60950203089a0876e18ab
Patricia Lojo <135821153+patilojo@users.noreply.github.com>
2024-01-17 17:53:05 +0100

    1 rpueba css sin sass

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,13 @@
 import Menu from "../components/Menu";
 import { useState } from "react";
-
+import './RegisterPage.css'
 
 const RegisterPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordRepeat, setPasswordRepeat] = useState(""); // la opcion tipica de escribir 2 veces la contraseña
-  const [statusMessage, setStatusMessage] = useState(""); // mensaje para el cliente sin necesidad de ver la consola 
+  const [statusMessage, setStatusMessage] = useState(""); // mensaje para el cliente sin necesidad de ver la consola
 
   const registerUser = async (e) => {
     e.preventDefault();
@@ -40,81 +40,88 @@ const RegisterPage = () => {
         setPasswordRepeat("");
       } else {
         const body = await res.json();
-        console.log("Error de datos: ", body.error);// se puede borrar es para pruebas
-        setStatusMessage(body.error);// para que muestre al usuario cual es el error p.e ya existe usuario con ese email
+        console.log("Error de datos: ", body.error); // se puede borrar es para pruebas
+        setStatusMessage(body.error); // para que muestre al usuario cual es el error p.e ya existe usuario con ese email
       }
     } catch (error) {
       setStatusMessage("Error al conectar con la Db"); // se le comunicaria esta info al cliente, si el problema es en el fetch??
       console.error(error);
-      
     }
   };
 
-  const messageForUser  = statusMessage ? (
+  const messageForUser = statusMessage ? (
     //borrar luego este estilo es solo para pruebas en registro y q salga rojo o verde
-    <div style={{ color: statusMessage.includes("exito") ? "green" : "red" }}> 
+    <div style={{ color: statusMessage.includes("exito") ? "green" : "red" }}>
       {statusMessage}
     </div>
   ) : (
-    <div>Introduce los datos</div>
+    <div></div>
   );
 
   return (
     <>
       <Menu />
-      <div>Pagina Register</div>
-    
 
-      <form onSubmit={registerUser}>
-        <div>
-          <label htmlFor="name">Nombre</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Escriba su nombre"
-          />
+     
+      <div className="signupFrm">
+        <div className="wrapper">
+          <form onSubmit={registerUser} className="form">
+          <h1 className="title">Sign up</h1>
+          {messageForUser}
+            <div className="inputContainer">
+              <label htmlFor="name" className="label">Nombre</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={name}
+                className="input"
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Escriba su nombre"
+              />
+            </div>
+            <div className="inputContainer">
+              <label htmlFor="email" className="label">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={email}
+                className="input"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Escriba su correo electrónico"
+              />
+            </div>
+            <div className="inputContainer">
+              <label htmlFor="password" className="label">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={password}
+                className="input"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Escriba su contraseña"
+              />
+            </div>
+            <div className="inputContainer">
+              <label htmlFor="passwordRepeat" className="label">Repetir Password</label>
+              <input
+                type="password"
+                name="passwordRepeat"
+                id="passwordRepeat"
+                value={passwordRepeat}
+                className="input"
+                onChange={(e) => setPasswordRepeat(e.target.value)}
+                placeholder="Repita su contraseña"
+              />
+            </div>
+            <div>
+              <input type="submit" className="submitBtn"/>
+            </div>
+          </form>
         </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Escriba su correo electrónico"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Escriba su contraseña"
-          />
-        </div>
-        <div>
-          <label htmlFor="passwordRepeat">Repetir Password</label>
-          <input
-            type="password"
-            name="passwordRepeat"
-            id="passwordRepeat"
-            value={passwordRepeat}
-            onChange={(e) => setPasswordRepeat(e.target.value)}
-            placeholder="Repita su contraseña"
-          />
-        </div>
-        <div>
-          <input type="submit" />
-        </div>
-      </form>
-      {messageForUser }
+      </div>
     </>
   );
 };
