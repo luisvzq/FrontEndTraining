43fbaa3f097d92999740eb8df06f6a5ad705f6dd
Luis Díaz <ztmphoto@gmail.com>
2024-01-31 15:09:51 +0100

    HomePage ready (slider and flipcards working)

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,3 +1,4 @@
+import FlipCard from "../../components/FlipCard/FlipCard";
 import SimpleSlider from "../../components/HomeSlider/HomeSlider";
 // import Fade from "../../components/HomeSlider/HomeSlider";
 // import Slideshow from "../../components/HomeSlider/HomeSlider";
@@ -7,8 +8,9 @@ import "./HomePage.scss";
 const HomePage = () => {
   return (
     <div className="home">
+      <FlipCard />
       <section className="title-section">
-        <h1 className="main-title">WORKOUT APP</h1>
+        <h1 className="main-title">{import.meta.env.VITE_APP_NAME}</h1>
       </section>
       <SimpleSlider />
     </div>
