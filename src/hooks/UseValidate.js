40ae8bf04b3e4c4bd2eb742637f5f73ca9b0b56d
David <142534572+Barreira83@users.noreply.github.com>
2024-02-01 13:44:17 +0100

    Description more than 200 characters

--- src/hooks/UseValidate.js
+++ src/hooks/UseValidate.js
@@ -17,13 +17,7 @@ const UseValidate = (name, description, typology,muscular, setStatusMessage, set
             setStatusMessage("El campo nombre tiene un maximo de 50 caracteres")
             setShakeAnimation(true);
             return false
-        }
-        if(description.length>200){
-            // alert ("El campo descriccion tiene un maximo de 200 caracteres")
-            setStatusMessage("El campo descriccion tiene un maximo de 200 caracteres")
-            setShakeAnimation(true);
-            return false
-        }
+        }      
         if(typology.length>50){
             // alert ("El campo tipologia tiene un maximo de 50 caracteres")
             setStatusMessage("El tipologia nombre tiene un maximo de 50 caracteres")
